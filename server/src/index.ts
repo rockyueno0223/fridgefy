f78251965a8666472a1627ad72fc095e13889dbd
Evangelina Vega <evangelina1001@hotmail.com>
2025-01-21 13:48:36 -0800

    Connected to MONGODB

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 
 app.use(
@@ -9,10 +11,22 @@ app.use(
     origin: "http://localhost:5173"
   })
 );
-
 app.use(clerkMiddleware());
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`[server]: listening at PORT ${PORT}`);
-});
+
+//connecting mongo db 
+const MONGO_URI = process.env.DATABASE_URI!
+
+mongoose
+  .connect(MONGO_URI, { dbName: 'fridgefy' })
+  .then(() => {
+    console.log('Connect to MONGODB database')
+
+    const PORT = process.env.PORT
+    app.listen(PORT, () => {
+      console.log(`[server]: listening at PORT ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MONGODB database')
+  })
