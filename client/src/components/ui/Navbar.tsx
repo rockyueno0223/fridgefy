5d9297826651a1dcbb3ea9025f4d4f8420629c61
YiEnTsai-91 <yeenetsai@gmail.com>
2025-01-22 15:17:29 -0800

    Header: basic function& style clear

---
+++ client/src/components/ui/Navbar.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom"
+
+type Props = {
+    isScreen: boolean
+    isToggle: () => void
+}
+
+export const Navbar = ({ isScreen, isToggle }: Props) => {
+
+    return (
+        <ul className="flex flex-col gap-8 font-bold bg-green-500 p-5 w-screen md:w-full md:bg-transparent md:flex-row md:p-0 md:justify-end" >
+            <NavLink
+                onClick={isToggle}
+                to="/"
+                className={({ isActive }) => `${isActive ? "text-black" : "text-white "}`}>
+                <p>Home</p>
+            </NavLink>
+            <NavLink
+                to="recipes"
+                onClick={isToggle}
+                className={({ isActive }) => `${isActive ? "text-black" : "text-white "}`}>
+                <p>Recipes</p>
+            </NavLink>
+            <NavLink
+                to="shoppinglist"
+                onClick={isToggle}
+                className={({ isActive }) => `${isActive ? "text-black" : "text-white"}`}>
+                <p>Shopping List</p>
+            </NavLink>
+            {isScreen ? null :
+                <>
+                    <button type="button" onClick={isToggle} className="text-white text-left">My Fridge</button>
+                    <button type="button" onClick={isToggle} className="text-white text-left"> My Recipe</button>
+                </>
+            }
+        </ul>
+    )
+}
