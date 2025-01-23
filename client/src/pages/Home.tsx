126e9618f9d02661024f74b3c50d1a715d73457d
YiEnTsai-91 <yeenetsai@gmail.com>
2025-01-23 14:04:32 -0800

    home style improvement

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,10 +5,9 @@ import { NavLink } from "react-router-dom"
 
 export const Home = () => {
     return (
-        <div className="w-screen button-0 left-0 z-[-10] relative" >
-            <img src="https://images.unsplash.com/photo-1494859802809-d069c3b71a8a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="home-page" className="w-screen h-[calc(100vh-74px)] object-cover" />
-            <div className="absolute top-20 left-20 flex flex-col gap-5 text-amber-950">
-                <div className=" dm-serif-text-home-title">
+        <>
+            <div className="w-max max-w-screen flex flex-col gap-8 text-amber-950 items-start pt-20 pl-10 lg:pl-0 pr-0 lg:pr-40 overflow-hidden">
+                <div className=" dm-serif-text-home-title flex-wrap">
                     <h1>Smart Meals</h1>
                     <h1>Effortless Cooking</h1>
                 </div>
@@ -22,18 +21,20 @@ export const Home = () => {
                         <SignInButton />
                     </SignedOut>
                     <SignedIn>
-                        <NavLink
-                            to="recipes"
-                            className="montserrat-home">
-                            <Button type="button">Start My Plan</Button>
-                        </NavLink>
+                        <div>
+                            <NavLink
+                                to="recipes"
+                                className="montserrat-home">
+                                <Button type="button">Start My Plan</Button>
+                            </NavLink>
+                        </div>
+
                     </SignedIn>
                 </div>
-
-
             </div>
 
-        </div>
+            <img src="https://images.unsplash.com/photo-1494859802809-d069c3b71a8a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="home-page" className="w-screen h-full object-cover top-[74px] left-0 fixed z-[-10]" />
+        </>
     )
 }
 
