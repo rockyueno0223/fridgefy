564a81029bab63c714c2bd3130bde3eaa2e9739d
Evangelina <47020897+evangclina@users.noreply.github.com>
2025-01-23 17:23:15 -0800

    Revert "Server setup" (#33)

--- server/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { recipesRouter } from "./recipes.router";
-
-export const v1router = Router()
-
-// /api/v1/recipes
-v1router.use("/recipes", recipesRouter)
