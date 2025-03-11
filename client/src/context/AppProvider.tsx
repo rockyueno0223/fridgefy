48c59e61470c17fa66caf8583ed4a990954961b7
Hiroki Ueno <rockyueno@gmail.com>
2025-03-11 12:58:06 -0700

    fix api url in app context

--- client/src/context/AppProvider.tsx
+++ client/src/context/AppProvider.tsx
@@ -1,8 +1,8 @@
-import { AppContext } from "@/context/AppContext";
-import { IRecipe } from "@/types/recipe";
-import { IUser } from "@/types/user";
-import { useAuth, useUser } from "@clerk/clerk-react";
-import { useEffect, useState } from "react";
+import { AppContext } from '@/context/AppContext';
+import { IRecipe } from '@/types/recipe';
+import { IUser } from '@/types/user';
+import { useAuth, useUser } from '@clerk/clerk-react';
+import { useEffect, useState } from 'react';
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const { user: clerkUser } = useUser();
@@ -18,7 +18,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [loadingRecipes, setLoadingRecipes] = useState<boolean>(false);
   const [recipesError, setRecipesError] = useState<string | null>(null);
 
-  const [wishlist, setWishlist] = useState<IUser["wishlist"]>([]);
+  const [wishlist, setWishlist] = useState<IUser['wishlist']>([]);
 
   useEffect(() => {
     if (clerkUser) {
@@ -26,16 +26,13 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
         try {
           setLoadingUser(true);
 
-          const res = await fetch(
-            `${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me`,
-            {
-              headers: {
-                Authorization: `Bearer ${await getToken()}`,
-              },
-            }
-          );
+          const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me`, {
+            headers: {
+              Authorization: `Bearer ${await getToken()}`,
+            },
+          });
           if (!res.ok) {
-            throw new Error("User not found");
+            throw new Error('User not found');
           }
           const data = await res.json();
 
@@ -43,7 +40,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
           setWishlist(data.wishlist);
           setUserError(null);
         } catch (error) {
-          console.error("Error fetching user", error);
+          console.error('Error fetching user', error);
           setUserError((error as Error).message);
         } finally {
           setLoadingUser(false);
@@ -60,17 +57,15 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
       try {
         setLoadingRecipes(true);
 
-        const res = await fetch(
-          `${import.meta.env.VITE_BACKEND_URL}/api/v1/recipes`
-        );
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/recipes`);
         if (!res.ok) {
-          throw new Error("Recipes not found");
+          throw new Error('Recipes not found');
         }
         const data = await res.json();
         setRecipes(data);
         setRecipesError(null);
       } catch (error) {
-        console.error("Error fetching recipes", error);
+        console.error('Error fetching recipes', error);
         setRecipesError((error as Error).message);
       } finally {
         setLoadingRecipes(false);
@@ -80,23 +75,20 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   }, []);
 
   const addToCart = async (ingredientIds: string[]) => {
-    const checkedIds = checkUniqueCart(ingredientIds)
+    const checkedIds = checkUniqueCart(ingredientIds);
     if (checkedIds.length === 0) {
-      return
+      return;
     }
 
     try {
-      const res = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/cart/add`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${await getToken()}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ingredientIds: checkedIds }),
-        }
-      );
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/cart/add`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ingredientIds: checkedIds }),
+      });
       const data = await res.json();
       if (data.success) {
         setUser((prev) => {
@@ -110,23 +102,20 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
         console.log(data.message);
       }
     } catch (error) {
-      console.error("Error adding ingredient to cart", error);
+      console.error('Error adding ingredient to cart', error);
     }
   };
 
   const removeFromCart = async (ingredientIds: string[]) => {
     try {
-      const res = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/cart/remove`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${await getToken()}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ingredientIds }),
-        }
-      );
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/cart/remove`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ingredientIds }),
+      });
       const data = await res.json();
       if (data.success) {
         setUser((prev) => {
@@ -140,30 +129,27 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
         console.log(data.message);
       }
     } catch (error) {
-      console.error("Error removing ingredient from cart", error);
+      console.error('Error removing ingredient from cart', error);
     }
   };
 
   const addToFridge = async (ingredientIds: string[]) => {
-    const checkedIds = checkUniqueFridge(ingredientIds)
+    const checkedIds = checkUniqueFridge(ingredientIds);
     if (checkedIds.length === 0) {
-      return
+      return;
     }
 
     try {
-      const res = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/fridge/add`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${await getToken()}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ingredientIds: checkedIds }),
-        }
-      );
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/fridge/add`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ingredientIds: checkedIds }),
+      });
       const data = await res.json();
-      console.log("data", data);
+      console.log('data', data);
 
       if (data.success) {
         setUser((prev) => {
@@ -183,17 +169,14 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
 
   const removeFromFridge = async (ingredientIds: string[]) => {
     try {
-      const res = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/fridge/remove`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${await getToken()}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ingredientIds }),
-        }
-      );
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/fridge/remove`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ingredientIds }),
+      });
       const data = await res.json();
       if (data.success) {
         setUser((prev) => {
@@ -211,33 +194,23 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
-
   const checkUniqueFridge = (ingredientIds: string[]): string[] => {
     if (!user) {
-      console.log(`error: sth went wrong on check unique ingredients.`)
-      return []
+      console.log(`error: sth went wrong on check unique ingredients.`);
+      return [];
     }
-    const ids = ingredientIds.filter((ingredientId) =>
-      !user.fridge.some((item) => item._id === ingredientId)
-    )
-    return (
-      ids
-    );
+    const ids = ingredientIds.filter((ingredientId) => !user.fridge.some((item) => item._id === ingredientId));
+    return ids;
   };
-
 
   const checkUniqueCart = (ingredientIds: string[]): string[] => {
     if (!user) {
-      console.log(`error: sth went wrong on check unique ingredients.`)
-      return []
+      console.log(`error: sth went wrong on check unique ingredients.`);
+      return [];
     }
-    const ids = ingredientIds.filter((ingredientId) =>
-      !user.cart.some((item) => item._id === ingredientId))
-    return (
-      ids
-    );
+    const ids = ingredientIds.filter((ingredientId) => !user.cart.some((item) => item._id === ingredientId));
+    return ids;
   };
-
 
   // wishlist
   // Add to wishlist
@@ -247,63 +220,53 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
     }
 
     try {
-      const res = await fetch(
-        `http://localhost:3400/api/v1/users/me/wishlist/add`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${await getToken()}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ingredientId: recipeId }),
-        }
-      );
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/wishlist/add`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ingredientId: recipeId }),
+      });
       const data = await res.json();
       if (data.success) {
         setWishlist(() => data.wishlist);
 
-        const addedRecipe = data.wishlist.filter(
-          (recipe: IRecipe) => recipe._id === recipeId
-        );
+        const addedRecipe = data.wishlist.filter((recipe: IRecipe) => recipe._id === recipeId);
 
-        const itemsNoneDupliclatetoFridge = checkUniqueFridge(addedRecipe[0].ingredients)
+        const itemsNoneDupliclatetoFridge = checkUniqueFridge(addedRecipe[0].ingredients);
         addToCart(itemsNoneDupliclatetoFridge);
       } else {
         console.error(data.message);
       }
     } catch (error) {
-      console.error("Error adding to wishlist", error);
+      console.error('Error adding to wishlist', error);
     }
   };
 
   // Remove from wishlist
   const removeFromWishlist = async (recipeId: string) => {
     try {
-      const res = await fetch(
-        `${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/wishlist/remove`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${await getToken()}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ingredientId: recipeId }),
-        }
-      );
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/users/me/wishlist/remove`, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ingredientId: recipeId }),
+      });
       const data = await res.json();
       if (data.success) {
         setWishlist(data.wishlist);
 
-        const removedRecipe = wishlist.filter(
-          (recipe: IRecipe) => recipe._id === recipeId
-        );
+        const removedRecipe = wishlist.filter((recipe: IRecipe) => recipe._id === recipeId);
 
         if (removedRecipe) removeFromCart(removedRecipe[0].ingredients);
       } else {
         console.error(data.message);
       }
     } catch (error) {
-      console.error("Error removing from wishlist", error);
+      console.error('Error removing from wishlist', error);
     }
   };
   return (
@@ -322,7 +285,7 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
         wishlist,
         addToWishlist,
         removeFromWishlist,
-        checkUniqueCart
+        checkUniqueCart,
       }}
     >
       {children}
