4bdc5fba2ec7a19fcbd522d3a4d7e8b9d929d4cd
Hiroki Ueno <rockyueno@gmail.com>
2025-03-10 22:34:10 -0700

    remove unnecessary code from Shopping List page

--- client/src/pages/ShoppingList.tsx
+++ client/src/pages/ShoppingList.tsx
@@ -1,12 +1,6 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
-import { useAppContext } from "@/context/AppContext";
-import { useEffect, useState } from "react";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { useAppContext } from '@/context/AppContext';
 
 // Define the IIngredient interface
 export interface IIngredient {
@@ -33,30 +27,10 @@ export interface IRecipe {
 }
 
 export const ShoppingList = () => {
-  const [ingredients, setIngredients] = useState<IIngredient[]>([]);
-  const { wishlist, loadingRecipes, recipesError, removeFromWishlist } =
-    useAppContext();
+  const { wishlist, loadingRecipes, recipesError, removeFromWishlist } = useAppContext();
 
   // Use recipes from context
   const storedRecipes = wishlist as IRecipe[];
-
-  useEffect(() => {
-    // Combine all ingredients from storedRecipes into a single array
-    const newArray: IIngredient[] = [];
-    storedRecipes?.forEach((recipe: IRecipe) => {
-      recipe.ingredients.forEach((ingredient: IIngredient) => {
-        newArray.push(ingredient);
-      });
-    });
-
-    setIngredients(newArray);
-  }, [storedRecipes]);
-
-  // Remove a recipe from the list
-  const handleRemove = (_id: string) => {
-    // Update the recipes state in the context (assuming you have a function to do this)
-    console.log("Remove recipe with ID:", _id);
-  };
 
   if (loadingRecipes) {
     return <div>Loading recipes...</div>;
@@ -67,16 +41,16 @@ export const ShoppingList = () => {
   }
 
   return (
-    <div className="accordion-container max-w-screen w-screen px-16 pt-10 md:w-max">
-      <h1 className="font-bold pb-5 text-xl">My Recipes</h1>
+    <div className='accordion-container max-w-screen w-screen px-16 pt-10 md:w-max'>
+      <h1 className='font-bold pb-5 text-xl'>My Recipes</h1>
 
       {storedRecipes?.map((recipe) => (
-        <Accordion key={recipe._id} type="single" collapsible>
+        <Accordion key={recipe._id} type='single' collapsible>
           <AccordionItem value={recipe._id}>
             <AccordionTrigger>{recipe.name}</AccordionTrigger>
             <AccordionContent>
-              <div className="flex flex-col gap-5">
-                <ul className="flex flex-col gap-3">
+              <div className='flex flex-col gap-5'>
+                <ul className='flex flex-col gap-3'>
                   {recipe.ingredients.map((ingredient, index) => (
                     <li key={`${recipe._id}-${index}`}>{ingredient.name}</li>
                   ))}
